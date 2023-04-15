2756a6aca0de3743c44c3f867d10d167da9b8ecc
kokilaAravindhan <125110809+kokilaAravindhan@users.noreply.github.com>
2023-04-15 19:01:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function foo(){
+var firstName=document.getElementById("firstNameText").value;
+var middleName=document.getElementById("middleNameText").value;
+var lastName=document.getElementById("lastNameText").value;
+var email=document.getElementById("email").value
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(email);
+
+}
